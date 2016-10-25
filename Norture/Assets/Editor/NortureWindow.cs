575991856c729ff03b25f612abc2eb590c01e21b
Peter Bay Bastian <peter.bastian@gmail.com>
2016-10-25 11:18:44 +0200

    .

--- Norture/Assets/Editor/NortureWindow.cs
+++ Norture/Assets/Editor/NortureWindow.cs
@@ -8,20 +8,18 @@ namespace Norture
     public class NortureWindow : EditorWindow
     {
         PreviewRenderUtility previewRenderUtility;
-
         Mesh sphereMesh;
-
         Material material;
-
         Cubemap cubemap;
-
         [SerializeField]
         DragController dragController;
-
         [SerializeField]
         Vector2 cameraDrag;
-
         Vector2 lastPosition;
+        Texture previewTexture;
+        Rect previewRect;
+        bool previewDirty = false;
+        const int cubemapResolution = 256;
 
         [MenuItem("Window/Norture")]
         public static void ShowWindow()
@@ -29,28 +27,33 @@ namespace Norture
             GetWindow<NortureWindow>("Norture");
         }
 
+        void Update()
+        {
+            if (previewDirty)
+            {
+                previewRenderUtility.BeginPreview(previewRect, GUIStyle.none);
+                previewRenderUtility.DrawMesh(sphereMesh, Matrix4x4.identity, material, 0);
+
+                previewRenderUtility.m_Camera.transform.position = Vector2.zero;
+                previewRenderUtility.m_Camera.transform.rotation = Quaternion.Euler(dragController.Position.y, dragController.Position.x, 0);
+                previewRenderUtility.m_Camera.transform.position = previewRenderUtility.m_Camera.transform.forward * -6f;
+                previewRenderUtility.m_Camera.Render();
+
+                previewTexture = previewRenderUtility.EndPreview();
+                previewDirty = false;
+            }
+        }
+
         void OnGUI()
         {
-            var rect = GUILayoutUtility.GetRect(position.width, position.width);
+            previewRect = GUILayoutUtility.GetRect(position.width, position.width);
             // var rect = new Rect(0, 0, position.width, position.height);
 
-            dragController.Sample(rect);
-            HandleClick(rect, previewRenderUtility.m_Camera);
+            dragController.Sample(previewRect);
+            HandleClick(previewRect, previewRenderUtility.m_Camera);
+            previewDirty = true;
 
-
-            previewRenderUtility.BeginPreview(rect, GUIStyle.none);
-            previewRenderUtility.DrawMesh(sphereMesh, Matrix4x4.identity, material, 0);
-
-            previewRenderUtility.m_Camera.transform.position = Vector2.zero;
-            previewRenderUtility.m_Camera.transform.rotation = Quaternion.Euler(dragController.Position.y, dragController.Position.x, 0);
-            previewRenderUtility.m_Camera.transform.position = previewRenderUtility.m_Camera.transform.forward * -6f;
-            previewRenderUtility.m_Camera.Render();
-
-
-
-            Texture resultRender = previewRenderUtility.EndPreview();
-
-            GUI.DrawTexture(rect, resultRender, ScaleMode.StretchToFill, false);
+            GUI.DrawTexture(previewRect, previewTexture, ScaleMode.StretchToFill, false);
         }
 
         void HandleClick(Rect rect, Camera camera)
@@ -59,21 +62,27 @@ namespace Norture
             var controlId = GUIUtility.GetControlID("Slider".GetHashCode(), FocusType.Passive);
             var eventType = current.GetTypeForControl(controlId);
 
-            if (eventType == EventType.MouseDown && current.modifiers == EventModifiers.None)
+            var correctEvent = (eventType == EventType.MouseDown || eventType == EventType.MouseDrag) && current.modifiers == EventModifiers.None;
+
+            if (correctEvent)
             {
-                var style = new GUIStyle();
                 current.Use();
                 lastPosition = current.mousePosition;
-                lastPosition.x = 2f * lastPosition.x / rect.width;
-                lastPosition.y = 2f * (rect.height - lastPosition.y) / rect.height;
+                lastPosition.x = camera.pixelWidth * lastPosition.x / rect.width;
+                lastPosition.y = camera.pixelHeight * (rect.height - lastPosition.y) / rect.height;
             }
-
 
             var cameraPosition = camera.gameObject.transform.position;
             var wsPosition = camera.ScreenToWorldPoint(new Vector3(lastPosition.x, lastPosition.y, camera.nearClipPlane));
             var wsDirection = (wsPosition - cameraPosition).normalized;
             Vector3 intersection;
             LineSphereIntersection(cameraPosition, wsDirection, out intersection);
+            
+            if (correctEvent)
+            {
+                SetCubemapPixelByDirection(cubemap, intersection.normalized, Color.black);
+                cubemap.Apply();
+            }
 
             EditorGUILayout.LabelField("Camera Pixel Size", string.Format("({0}, {1})", camera.pixelWidth, camera.pixelHeight));
             EditorGUILayout.LabelField("Camera World Space", cameraPosition.ToString());
@@ -81,8 +90,70 @@ namespace Norture
             EditorGUILayout.LabelField("World Space", wsPosition.ToString());
             EditorGUILayout.LabelField("Direction", wsDirection.ToString());
             EditorGUILayout.LabelField("Intersection", intersection.ToString());
+        }
 
+        void SetCubemapPixelByDirection(Cubemap cubemap, Vector3 direction, Color color)
+        {
+            float u, v;
+            var face = CalculateCubemapCoordinates(direction, out u, out v);
+            cubemap.SetPixel(face, (int)(u*cubemap.width), (int)(v*cubemap.width), color);
+        }
 
+        CubemapFace CalculateCubemapCoordinates(Vector3 direction, out float u, out float v)
+        {
+            var absX = Mathf.Abs(direction.x);
+            var absY = Mathf.Abs(direction.y);
+            var absZ = Mathf.Abs(direction.z);
+
+            CubemapFace face;
+
+            if (absX > absY && absX > absZ)
+            {
+                if (direction.x > 0)
+                {
+                    face = CubemapFace.PositiveX;
+                    CalculateUV(-direction.z, -direction.y, absX, out u, out v);
+                }
+                else
+                {
+                    face = CubemapFace.NegativeX;
+                    CalculateUV(direction.z, -direction.y, absX, out u, out v);
+                }
+            }
+            else if (absY > absX && absY > absZ)
+            {
+                if (direction.y > 0)
+                {
+                    face = CubemapFace.PositiveY;
+                    CalculateUV(direction.x, direction.z, absY, out u, out v);
+                }
+                else
+                {
+                    face = CubemapFace.NegativeY;
+                    CalculateUV(direction.x, -direction.z, absY, out u, out v);
+                }
+            }
+            else
+            {
+                if (direction.z > 0)
+                {
+                    face = CubemapFace.PositiveZ;
+                    CalculateUV(direction.x, -direction.y, absZ, out u, out v);
+                }
+                else
+                {
+                    face = CubemapFace.NegativeZ;
+                    CalculateUV(-direction.x, -direction.y, absZ, out u, out v);
+                }
+            }
+
+            return face;
+        }
+
+        void CalculateUV(float sc, float tc, float absMa, out float u, out float v)
+        {
+            u = ((sc/absMa) + 1f) * 0.5f;
+            v = ((tc/absMa) + 1f) * 0.5f;
         }
 
         bool LineSphereIntersection(Vector3 origin, Vector3 direction, out Vector3 intersection)
@@ -109,17 +180,24 @@ namespace Norture
             sphereMesh = GetSphereMesh();
             material = LoadMaterial();
             cubemap = CreateCubemap();
+            InitializeCubemap(cubemap);
             material.SetTexture("_Cube", cubemap);
+            previewTexture = new Texture2D(0, 0);
+            previewRect = new Rect(Vector2.zero, new Vector2(cubemapResolution, cubemapResolution));
+            previewDirty = true;
+        }
 
+        void InitializeCubemap(Cubemap cubemap)
+        {
             var faces = new[] { CubemapFace.PositiveX, CubemapFace.NegativeX, CubemapFace.PositiveY, CubemapFace.NegativeY, CubemapFace.PositiveZ, CubemapFace.NegativeZ };
             var colors = new[] { new Color(1, 0, 0), new Color(1, 1, 0), new Color(0, 1, 0), new Color(0, 1, 1), new Color(0, 0, 1), new Color(1, 0, 1) };
             int i = 0;
 
             foreach (var face in faces)
             {
-                for (int x = 0; x < 128; ++x)
+                for (int x = 0; x < cubemapResolution; ++x)
                 {
-                    for (int y = 0; y < 128; ++y)
+                    for (int y = 0; y < cubemapResolution; ++y)
                     {
                         cubemap.SetPixel(face, x, y, colors[i]);
                     }
@@ -151,7 +229,7 @@ namespace Norture
 
         Cubemap CreateCubemap()
         {
-            return new Cubemap(128, TextureFormat.ARGB32, false);
+            return new Cubemap(cubemapResolution, TextureFormat.ARGB32, false);
         }
     }
 }
